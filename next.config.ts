045631dc1664efbdaa53b3574f03f9a5c7fa7cf2
Antonymm225 <amachuca330@gmail.com>
2026-06-26 11:28:09 -0500

    inmagen

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  async headers() {
+    return [
+      {
+        source: "/images/Ecomfy-Lead-Logo.png",
+        headers: [
+          {
+            key: "Content-Disposition",
+            value: "inline",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
